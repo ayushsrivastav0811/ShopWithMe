e8e298013e1341c60ca80b0b05dc01adba147ca3
Ayush <ayush08nov2000@gmail.com>
2023-05-28 14:45:45 +0530

    inital commit

---
+++ frontend/src/component/layout/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import playStore from "../../../images/playstore.png";
+import appStore from "../../../images/Appstore.png";
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <footer id='footer'>
+        <div className="midFooter">
+            <h1>ShopWithMe</h1>
+            <p>Best Quality with Best Services</p>
+
+            <p>Copyrights 2021 &copy; ShopWithMe</p>
+        </div>
+        <div className="rightFooter">
+            <h4>Follow Us</h4>
+            <a href='https://www.instagram.com/'>Instagram</a>
+            <a href='https://twitter.com/'>Twitter</a>
+            <a href='https://www.youtube.com/'>Youtube</a>
+            
+        </div>
+
+    </footer>
+    
+  )
+}
+
+export default Footer
